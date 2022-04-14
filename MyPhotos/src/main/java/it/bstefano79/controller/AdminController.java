bbc89d5d99086914fcbade148fa56511db5fcdfc
Stefano Belli <56109150+bstefano79@users.noreply.github.com>
2022-04-14 14:17:38 +0200

    inserimento method GET

--- MyPhotos/src/main/java/it/bstefano79/controller/AdminController.java
+++ MyPhotos/src/main/java/it/bstefano79/controller/AdminController.java
@@ -2,11 +2,12 @@ package it.bstefano79.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminController {
 	
-	@RequestMapping("/admin")
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
 	public String admin()
 	{
 		return "admin";
--- MyPhotos/src/main/java/it/bstefano79/controller/HomeController.java
+++ MyPhotos/src/main/java/it/bstefano79/controller/HomeController.java
@@ -3,18 +3,19 @@ package it.bstefano79.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index()
 	{
 		return "index";
 	}
 	
-	@RequestMapping("/hello/{name}")
+	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
 	@ResponseBody
 	public String Hello(@PathVariable String name)
 	{

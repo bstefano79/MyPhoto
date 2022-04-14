801927a0b95c8839470e6030e866d0b472064960
Stefano Belli <56109150+bstefano79@users.noreply.github.com>
2022-04-14 10:03:01 +0200

    first commit - inizializzazione progetto

---
+++ MyPhotos/src/test/java/it/bstefano79/MyPhotosApplicationTests.java
@@ -0,0 +1,13 @@
+package it.bstefano79;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
